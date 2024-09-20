57b8e3dc2edc70a26bf064d89159ffd5338361d4
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-09-20 22:54:53 +0100

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,32 +6,29 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-4 fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">Graso</div>
+        <div className="text-white text-lg font-bold">Brand</div>
 
-
-        <div className="hidden md:flex space-x-6">
+        <div className="hidden md:flex space-x-6 items-center">
           <a href="#home" className="text-white hover:text-gray-300">
             Home
           </a>
           <a href="#about" className="text-white hover:text-gray-300">
-            About
+            Explore
           </a>
           <a href="#services" className="text-white hover:text-gray-300">
-            Services
+            Portfolio
           </a>
           <a href="#contact" className="text-white hover:text-gray-300">
-            Contact
+            Whitepaper
           </a>
+          {/* Get Started Button for Desktop */}
+          <div>
+            <button className="bg-[#24C2A5] text-white px-4 py-2 rounded-md font-semibold">
+              Get started
+            </button>
+          </div>
         </div>
 
-
-        <div>
-        <button className="bg-[#24C2A5] text-white px-4 py-2 rounded-md font-semibold desktop-btn">
-          Get started
-        </button>
-      </div>
-
-      
         {/* Mobile menu button */}
         <div className="md:hidden">
           <button
@@ -55,29 +52,26 @@ const Navbar = () => {
           </button>
         </div>
       </div>
+
       {/* Mobile Menu */}
       <div
         className={`md:hidden bg-blue-500 overflow-hidden transition-all duration-500 ease-in-out ${
-          isOpen ? "max-h-40" : "max-h-0"
+          isOpen ? "max-h-60" : "max-h-0"
         }`}
       >
         <a href="#home" className="block text-white py-2">
           Home
         </a>
         <a href="#about" className="block text-white py-2">
-          About
+          Explore
         </a>
         <a href="#services" className="block text-white py-2">
-          Services
+          Portfolio
         </a>
         <a href="#contact" className="block text-white py-2">
-          Contact
+            Whitepaper
         </a>
-        <a href="#contact" className="block text-white py-2">
-          Get Started 
-               </a>
-        
-      </div>
+    </div>
     </nav>
   );
 };
