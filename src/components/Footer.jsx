d7292fafe44d9f1362265662a3c78f82560a4435
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-22 21:05:20 +0100

    [feat: width adjust]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="flex flex-col p-16 font-[Montserrat] max-md:p-6 bg-gradient-to-r from-[#f9f4fb] to-[#fffaec]">
       {/* Up part */}
-      <div className="flex justify-between items-start gap-10 max-md:flex-col max-md:gap-6">
+      <div className="flex justify-between items-start gap-16 max-md:flex-col max-md:gap-6">
         <div className="flex flex-col space-y-2 flex-1 min-w-[200px]">
           <h2 className="font-semibold text-[2rem]">Graso</h2>
           <div>
@@ -16,7 +16,7 @@ function Footer() {
           </div>
         </div>
 
-        <div className="flex justify-center items-start flex-1 space-x-12 max-md:flex-col max-md:gap-6 [&_h2]:text-[1.5rem]">
+        <div className="flex justify-center items-start flex-1 space-x-15 max-md:flex-col max-md:gap-6 [&_h2]:text-[1.5rem]">
           <div className="flex flex-col space-y-2 flex-1 min-w-[100px]">
             <h2 className="font-semibold">COMMUNITY</h2>
             <div className="flex flex-col space-y-2 [&_p]:cursor-pointer">
@@ -62,7 +62,7 @@ function Footer() {
         </div>
 
         <div className="w-full">
-          <ul className="flex gap-6 justify-center max-md:flex-col max-md:gap-3 [&_li]:cursor-pointer min-w-[300px]">
+          <ul className="flex gap-6 justify-center max-md:flex-col max-md:gap-3 [&_li]:cursor-pointer min-w-[500px]">
             <li>Home</li>
             <li>About Us</li>
             <li>Our Team</li>
