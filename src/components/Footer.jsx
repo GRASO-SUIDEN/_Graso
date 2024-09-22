964a8cc19032ae15dd9cea603d0bb98c0e093179
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-22 20:03:04 +0100

    [feat: width adjust]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="flex flex-col p-16 font-[Montserrat] max-md:p-6 bg-gradient-to-r from-[#f9f4fb] to-[#fffaec]">
       {/* Up part */}
       <div className="flex justify-between items-start space-x-[20rem]  max-md:flex-col max-md:space-x-0 max-md:space-y-3">
-        <div className="flex flex-col space-y-2 w-full max-w-full">
+        <div className="flex flex-col space-y-2 w-full min-w-96">
           <h2 className="font-semibold text-[2rem]">Graso</h2>
           <div>
             <p>
