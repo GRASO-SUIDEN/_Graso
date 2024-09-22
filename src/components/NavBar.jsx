6092e3bc1df0041f7055de2d6dcd80d7027f1b53
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-22 15:22:45 +0100

    navbar modified

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-center fixed top-0 left-0 justify-between px-8 py-4 w-full bg-white bg-opacity-70 backdrop-blur-md z-50">
+    <nav className="flex items-center fixed top-0 left-0 justify-between px-8 py-4 w-full bg-white bg-opacity-70 backdrop-blur-md z-50 font-[Montserrat]">
       <div className="flex-shrink-0">
         <h1 className="text-2xl text-[#24C2A5] font-bold">Graso</h1>
       </div>
@@ -21,7 +21,7 @@ function Navbar() {
             (item) => (
               <li
                 key={item}
-                className="relative cursor-pointer group max-md:w-[7rem] max-md:text-center max-md:hover:bg-[#24C2A5] max-md:hover:text-white max-md:hover:rounded-md"
+                className="relative  cursor-pointer group select-none max-md:w-[7rem] max-md:text-center max-md:hover:bg-[#24C2A5] max-md:hover:text-white max-md:hover:rounded-md"
               >
                 {item}
                 <span className="max-md:hidden absolute left-0 bottom-0 w-0 h-[3px] bg-[#24C2A5] transition-all duration-300 group-hover:w-full" />
@@ -32,7 +32,7 @@ function Navbar() {
       </div>
 
       <div className="max-md:flex max-md:-space-x-4">
-        <button className="bg-[#24C2A5] text-white px-4 py-2 rounded-md font-semibold max-md:ml-10">
+        <button className="bg-[#24C2A5] text-white px-4 py-2 rounded-md font-semibold max-md:ml-10 active:scale-90 hover:border-solid hover:bg-white hover:text-[#24C2A5] transition-all duration-300">
           Get started
         </button>
 
