288266bd987284d372917da83ace0bb1d0ef57de
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-22 20:26:40 +0100

    [feat: width adjust]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
           </div>
         </div>
 
-        <div className="flex justify-center items-start flex-1 space-x-7 max-md:flex-col max-md:gap-6 [&_h2]:text-[1.5rem]">
+        <div className="flex justify-center items-start flex-1 space-x-12 max-md:flex-col max-md:gap-6 [&_h2]:text-[1.5rem]">
           <div className="flex flex-col space-y-2 flex-1 min-w-[100px]">
             <h2 className="font-semibold">COMMUNITY</h2>
             <div className="flex flex-col space-y-2 [&_p]:cursor-pointer">
@@ -62,7 +62,7 @@ function Footer() {
         </div>
 
         <div className="w-full">
-          <ul className="flex gap-6 justify-center max-md:flex-col max-md:gap-3 [&_li]:cursor-pointer min-w-[200px]">
+          <ul className="flex gap-6 justify-center max-md:flex-col max-md:gap-3 [&_li]:cursor-pointer min-w-[300px]">
             <li>Home</li>
             <li>About Us</li>
             <li>Our Team</li>
