70c2291a8dd8fa752026248889f7c0cced4af608
chiscookeke11 <chiscookeke11@gmail.com>
2024-09-23 09:57:54 -1100

    added loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import Homepage from "./pages/home-page/HomePage";
-
+import LoadingPage from './pages/loading-page/LoadingPage';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <Homepage />
+      {loading ? <LoadingPage /> : <Homepage />}
     </div>
   );
 }
---
+++ src/pages/loading-page/LoadingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LoadingPage.css';
+
+const LoadingPage = () => {
+  return (
+    <div className="loading-container">
+    <span class="back">
+	<span>L</span>
+	<span>o</span>
+	<span>a</span>
+	<span>d</span>
+	<span>i</span>
+	<span>n</span>
+	<span>g</span>
+</span>
+    </div>
+  );
+};
+
+export default LoadingPage;
