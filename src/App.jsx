a53c41521b22300a0f426b25f95afaaa727246cc
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-23 23:51:04 +0100

    navbar modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Homepage from "./pages/home-page/HomePage";
-import LoadingPage from './pages/loading-page/LoadingPage';
+import LoadingPage from "./pages/loading-page/LoadingPage";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -8,16 +8,12 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 4000); 
+    }, 4000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <div>
-      {loading ? <LoadingPage /> : <Homepage />}
-    </div>
-  );
+  return <div>{loading ? <LoadingPage /> : <Homepage />}</div>;
 }
 
 export default App;
