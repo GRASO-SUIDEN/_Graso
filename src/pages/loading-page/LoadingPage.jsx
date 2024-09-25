e81379335fd6a7b985d80e082e897a79325ecf64
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-25 09:13:51 +0100

    changes

--- src/pages/loading-page/LoadingPage.jsx
+++ src/pages/loading-page/LoadingPage.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './LoadingPage.css';
+import "./LoadingPage.css";
 
 const LoadingPage = () => {
   return (
     <div className="loading-container">
-    <span class="back">
-	<span>L</span>
-	<span>o</span>
-	<span>a</span>
-	<span>d</span>
-	<span>i</span>
-	<span>n</span>
-	<span>g</span>
-</span>
+      <span className="back">
+        <span>L</span>
+        <span>o</span>
+        <span>a</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>g</span>
+      </span>
     </div>
   );
 };
