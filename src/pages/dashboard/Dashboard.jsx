029b04b8e5605e38d5768076b0f1de75af0b8a89
chiscookeke11 <chiscookeke11@gmail.com>
2024-09-26 04:14:04 -1100

    sidenav

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DashboardSidenav from '../../components/dashboard-sidenav/DashboardSidenav';
+import './dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div className='Dashboard-container'>
+        <DashboardSidenav/>
+    </div>
+  )
+}
+
+
+
+export default Dashboard;
+
