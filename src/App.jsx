a8e8bdb7caaa349525e7ecf09d4165a67736a5b0
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-26 12:22:23 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/GRASO-SUIDEN/_Graso""
    This reverts commit 893e35252075268da240923c0339c3a8e4169b7d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
 import Homepage from "./pages/home-page/HomePage";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 4000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return <div>{loading ? <LoadingPage /> : <Homepage />}</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
+
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 3000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return <div>{loading ? <LoadingPage /> : <Homepage />}</div>;
+// >>>>>>> fc01d3ea32ea10c4b81fd4b75ce5407f1e242ef9
+// }
 
 export default App;
