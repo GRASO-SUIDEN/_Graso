85cd16864c6d2d7320dda9567e2d01e7f6e7cbf6
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-09-28 20:27:57 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home-page/HomePage"
+import HomePage from "./pages/home-page/HomePage";
 import LoadingPage from "./pages/loading-page/LoadingPage";
 import Dashboard from "./components/dashboard/Dashboard";
 import AppLayout from "./pages/app-layout/AppLayout";
-
+import AboutUs from "./pages/about-us/AboutUs";
+import PageNotFound from "./pages/page-not-found/PageNotFound";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -17,19 +18,19 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={loading ? <LoadingPage /> : <HomePage/> } />
+        <Route index element={loading ? <LoadingPage /> : <HomePage />} />
+        <Route path="about-us" element={<AboutUs />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<Navigate replace to="dashboard" />} />
           <Route path="dashboard" element={<Dashboard />} />
         </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Navbar from '../../components/NavBar';
-import JoinUs from '../../components/JoinUs';
-import Footer from '../../components/Footer';
-import './about-us.css';
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Navbar from "../../components/navbar/NavBar";
+import JoinUs from "../../components/JoinUs";
+import Footer from "../../components/Footer";
+import "./about-us.css";
 
 const AboutUs = () => {
   useEffect(() => {
     AOS.init({
-      duration: 2000, 
-      once: true,     
+      duration: 2000,
+      once: true,
     });
   }, []);
 
   return (
-    <div className='about-us-page'>
+    <div className="about-us-page">
       <Navbar />
       <div className="section mt-16">
         <div className="info-box-text">
-          <h2 data-aos="fade-up">Providing the best future for your best living</h2>
+          <h2 data-aos="fade-up">
+            Providing the best future for your best living
+          </h2>
           <p data-aos="fade-up" data-aos-delay="300">
-            To revolutionize the global real estate market by making property ownership 
-            and investment opportunities available to everyone, regardless of financial background. 
-            We envision a world where anyone can build wealth through fractional real estate ownership,
-            while contributing to a more sustainable and equitable future.
+            To revolutionize the global real estate market by making property
+            ownership and investment opportunities available to everyone,
+            regardless of financial background. We envision a world where anyone
+            can build wealth through fractional real estate ownership, while
+            contributing to a more sustainable and equitable future.
           </p>
         </div>
       </div>
