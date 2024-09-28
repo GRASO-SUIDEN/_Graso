62cd002774d0b5ec4944ea42cc4fdf79010d85e2
Genesis ufedo <wisdomgenesis4@gmail.com>
2024-09-28 13:43:13 +0100

    Genesis Dashbord

--- src/pages/app-layout/AppLayout.jsx
+++ src/pages/app-layout/AppLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import AppSidenav from "../../components/app-sidenav/AppSidenav";
 function AppLayout() {
   return (
-    <div className="flex">
+    <div className="gird grid-cols-2 gap-10  ">
       <AppSidenav />
       <Outlet />
     </div>
