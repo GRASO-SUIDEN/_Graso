2e0f578a06c9f202b332becc4b2a1a72b0965c7d
Genesis ufedo <wisdomgenesis4@gmail.com>
2024-09-29 21:50:25 +0100

    genesis 4

--- src/pages/app-layout/AppLayout.jsx
+++ src/pages/app-layout/AppLayout.jsx
@@ -1,26 +1,16 @@
-// import { Outlet } from "react-router-dom";
-// <<<<<<< HEAD
-// import AppSidenav from "../../components/app-sidenav/AppSidenav"
-// import ExploreLand from "../exploreland/ExploreLand.JSX";
-// =======
-// import AppSidenav from "../../components/app-sidenav/AppSidenav";
-// import AppHeader from "../../components/app-header/AppHeader";
-// >>>>>>> d4658a177755b6557da6d37a030e6ba60f6285db
-// function AppLayout() {
-//   return (
-//     <div className="flex w-full h-[100vh]">
-//       <AppSidenav />
-// <<<<<<< HEAD
-//       <ExploreLand />
-//       <Outlet />
-// =======
+import { Outlet } from "react-router-dom";
+import AppSidenav from "../../components/app-sidenav/AppSidenav";
+import AppHeader from "../../components/app-header/AppHeader";
+function AppLayout() {
+  return (
+    <div className="flex w-full h-[100vh]">
+      <AppSidenav />
 
-//       <div className="flex flex-col flex-grow">
-//         <AppHeader />
-//         <Outlet />
-//       </div>
-// >>>>>>> d4658a177755b6557da6d37a030e6ba60f6285db
-//     </div>
-//   );
-// }
-// export default AppLayout;
+      <div className="flex flex-col flex-grow">
+        <AppHeader />
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+export default AppLayout;
