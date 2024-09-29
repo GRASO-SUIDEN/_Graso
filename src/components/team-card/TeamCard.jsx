00dcbd11328c6011889c882c2ee78c55a215a158
chiscookeke11 <chiscookeke11@gmail.com>
2024-09-29 00:36:51 -1100

    team details

--- src/components/team-card/TeamCard.jsx
+++ src/components/team-card/TeamCard.jsx
@@ -8,19 +8,19 @@ import './team-card.css'
 
         <div class="card">
 		<div class="image-container">
-			<img src="https://github.com/chiscookeke11/Test-pagea/blob/main/sui_logo.png?raw=true" alt="" />
+			<img src="https://github.com/chiscookeke11/Test-pagea/blob/main/chidile.jpg?raw=true" alt="image" />
 		</div>
 		<div class="content">
-			<h2 class="profile-name">Name</h2>
-			<p class="description">description...</p>
+			<h2 class="profile-name">Chidile Ozoemena</h2>
+			<p class="description">Team Lead</p>
 		</div>
 		<div class="social-links">
-        <a href="#" className="linkedin">
+        <a href="https://linkedin.com/in/chidile-ozoemena-293688231/" className="linkedin">
   <img src="https://raw.githubusercontent.com/chiscookeke11/Test-pagea/e56603dfe7ff3825fbe39413af7ab4ff8dfd5ab2/linkedin.svg" alt="socials" />
 </a>
-<a href="#" class="twitter">
+<a href="https://x.com/0xAfroTechBoss" class="twitter">
 				<img src="https://raw.githubusercontent.com/chiscookeke11/Test-pagea/e56603dfe7ff3825fbe39413af7ab4ff8dfd5ab2/twitter-x.svg" alt="social" />
-			</a><a href="#" class="instagram">
+			</a><a href="https://github.com/AfroTechBoss" class="instagram">
 				<img src="https://raw.githubusercontent.com/chiscookeke11/Test-pagea/e56603dfe7ff3825fbe39413af7ab4ff8dfd5ab2/github%20(6).svg" alt="socials"/>
 			</a>
 		</div>
