900a83027d73e20a96fc5b49cf2f93b70ff79253
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-10-01 11:30:04 +0100

    [fix: styles conflict]

--- src/components/explore-land/ExploreLand.jsx
+++ src/components/explore-land/ExploreLand.jsx
@@ -1,36 +1,40 @@
-import { ExploreCard } from "../explore/Explore";
-import "./Explore-land.css"
+import ExploreCard from "../explore/ExploreCard";
+import styles from "./ExploreLand.module.css";
 
 function ExploreLand() {
   return (
     <div>
       {/* <ul className="text">
-        <li><h1>Explore land</h1></li>
-        <li className=" pl-10 " >Graso {">"} <span> properties</span></li>
+        <li>
+          <h1>Explore land</h1>
+        </li>
+        <li className=" pl-10 ">
+          Graso {">"} <span> properties</span>
+        </li>
       </ul> */}
 
-      <div className="container ">
-        <div className="   ">
+      <div className={styles.container}>
+        <div className="">
           {" "}
           <ExploreCard />{" "}
         </div>
-        <div className="item">
+        <div className={styles.item}>
           {" "}
           <ExploreCard />{" "}
         </div>
-        <div className="item">
+        <div className={styles.item}>
           {" "}
           <ExploreCard />{" "}
         </div>
-        <div className="item">
+        <div className={styles.item}>
           {" "}
           <ExploreCard />{" "}
         </div>
-        <div className="item">
+        <div className={styles.item}>
           {" "}
           <ExploreCard />{" "}
         </div>
-        <div className="item">
+        <div className={styles.item}>
           {" "}
           <ExploreCard />{" "}
         </div>
---
+++ src/components/explore/ExploreCard.jsx
@@ -0,0 +1,44 @@
+import Landsite from "../../assets/landsite.jpg";
+import Usdt from "../../assets/usdt-img.png";
+import Timer from "../Timer";
+
+export default function ExploreCard({ data }) {
+  return (
+    <div className="flex flex-col justify-between rounded-t-[2.1rem] rounded-b-3xl bg-[#24C2A5] w-[20rem] h-[32rem] space-y-1 min-w-[250px]">
+      <div className="bg-white rounded-b-3xl rounded-t-[2rem] flex-grow overflow-hidden">
+        <div className="p-4 w-full h-full flex flex-col justify-between">
+          <img src={Landsite} className="w-full h-40 object-cover" />
+          <div className="flex-grow">
+            <h1 className="font-bold pt-4 text-2xl">{data?.name}</h1>
+            <h2 className="text-gray-400">{data?.location}</h2>
+            <small className="font-sans leading-1 text-xs font-small">
+              {data?.description}
+            </small>
+          </div>
+
+          <div className="mt-3">
+            <div className="flex justify-between items-center">
+              <small>Targeted Raise</small>
+              <div className="flex mb-2">
+                <img src={Usdt} className="w-6 h-6" />
+                <h2>200,000</h2>
+              </div>
+            </div>
+            <progress max="15" value="7.5" />
+          </div>
+
+          <div className="flex justify-between items-center gap-4 pt-4">
+            <div>
+              <small className="font-semibold">Min.Entry</small>
+              <h1 className="text-teal-500 font-semibold">5 USDT</h1>
+            </div>
+            <button className="bg-[#24c2a5] w-auto px-4 py-2 rounded-full text-white hover:border-solid hover:bg-white hover:text-[#24C2A5] transition-all duration-300">
+              INVEST
+            </button>
+          </div>
+        </div>
+      </div>
+      <Timer />
+    </div>
+  );
+}
