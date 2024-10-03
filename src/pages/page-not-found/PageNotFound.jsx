a81362e4aad030c137e9efb4c3767471c1353b70
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-10-03 13:38:56 +0100

    [feat: 404 page modified]

--- src/pages/page-not-found/PageNotFound.jsx
+++ src/pages/page-not-found/PageNotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function PageNotFound() {
   return (
     <div className="flex flex-col justify-center items-center text-center font-bold h-[100vh] space-y-4 bg-[#E6F7F8]">
-      <h1 className="text-[2rem]">404</h1>
+      <h1 className="text-[5rem]">404</h1>
       <p className="text-[1.2rem]">
         Don&apos;t Worry You are not lost. It&apos;s just the page that ran into
         problems.
